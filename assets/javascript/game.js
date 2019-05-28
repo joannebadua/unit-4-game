bd6a6309b473be597a18c1bafb0f483395365143
Joanne Badua <joannebadua@Joannes-MacBook-Air.local>
2019-05-28 08:38:30 -0700

    found another forloop method used to generate random numbers in a range between 0  to 10, where 0 is inclusive and 10 is exclusive. This code uses Math.random() method, which returns pseudo-random number in a range 0.0 to 1.0, where later is exclusive, by multiplying output with and then type casting into int, we can generate random integers in any range. javarevisited.blogspot

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,20 @@
     
     // Computer creates a random number (19-120) as a goal for the player to match 
     // with accumulated points
-    var Random = [];
-    for (var i = 19; i <= 120; i++){
-      numbers.push(i)
-      // console.log()
-    }
-    $('#randomNumber').text(Random);
+    // var Random = [];
+    // for (var i = 19; i <= 120; i++){
+    //   numbers.push(i)
+    //   console.log(numbers[i])
+    // }
+    // $('#randomNumber').text(Random);
 
 
+    for(int i =0; i<3; i++){
+        int randomInt = (int)(10.0 * Math.random());
+        System.out.println("pseudo random int between 1 and 10 : " + randomInt );
+  }
+  
 
+    // $(".bin").on("click", function () {
 
-    $(".bin").on("click", function () {
-
-    });
+    // }
