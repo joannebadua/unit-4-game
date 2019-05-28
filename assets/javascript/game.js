bd2aff195ee77047b322d8b297ac516791c5ba56
Joanne Badua <joannebadua@Joannes-MacBook-Air.local>
2019-05-28 09:25:09 -0700

    delete unnecessary comments for generating random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,24 +8,6 @@
     // with accumulated points
     var Random = Math.floor(Math.random() * 120)+19;
     $('#randomNumber').text(Random);
-    // var Random = [];
-    // for (var i = 19; i <= 120; i++){
-    //   numbers.push(i)
-    //   console.log(numbers[i])
-    // }
-
-
-
-//     for(int i =0; i<3; i++){
-//         int randomInt = (int)(10.0 * Math.random());
-//         System.out.println("pseudo random int between 1 and 10 : " + randomInt );
-// //   }
-// w3 schools random integer
-// document.getElementById("randomNumber").innerHTML =
-// Math.floor(Math.random() * 120)+19;
-
-
-
     $("#binOne").on("click", function () {
 // need to add results when binOneTwoThreeFour clicked such as 
 // 1) adding Score inside and on the display.
