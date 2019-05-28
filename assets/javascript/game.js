896fcaa0c178023d115141a7f9950c99511ae202
Joanne Badua <joannebadua@Joannes-MacBook-Air.local>
2019-05-28 10:48:55 -0700

    console.log attribute, and duplicate attr for bins 2-4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,20 @@ $(document).ready(function () {
     // Computer creates a random number (19-120) as a goal for the player to match 
     // with accumulated points, then displays Random variable in text connected
     // to html id randomNumber
-    var Random = Math.floor(Math.random() * 101+19);
+    var Random = Math.floor(Math.random() * 101 + 19);
     $('#randomNumber').text(Random);
-    var recycleRandom = Math.floor(Math.random() * 12+1);
+    var recycleRandom = Math.floor(Math.random() * 12 + 1);
+    console.log(recycleRandom);
+    // even without the adding, build by brick the attr. for each bin
+    $("#binOne").attr("num", recycleRandom);
+    $("#binTwo").attr("num", recycleRandom);
+    $("#binThree").attr("num", recycleRandom);
+    $("#binFour").attr("num", recycleRandom);
 
     $("#binOne").on("click", function () {
+        // *tip: console.log to see if it works.
+        console.log("wow", $(this).attr("num"));
+        // *tip: now console.log the attr. number
         // need to add results/whats going to happen when binOneTwoThreeFour clicked such as 
         // 1) adding Score inside and on the display.
         // 2) determining if this Score matches the Goal number
@@ -24,4 +33,4 @@ $(document).ready(function () {
 
     // reset
 
-    });
+});
