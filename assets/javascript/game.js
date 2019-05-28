b4351977615435fc0a49224155ae2a42b7599809
Joanne Badua <joannebadua@Joannes-MacBook-Air.local>
2019-05-28 08:19:47 -0700

    transfer javascript from html to js file, and link rel to html, also resetcss

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+  $(document).ready(function () {
+    // var firstBin = $("#1-paper");
+    // var buttonValue =;
+    // var userScore = "";
+    
+    // Computer creates a random number (19-120) as a goal for the player to match 
+    // with accumulated points
+    var Random = [];
+    for (var i = 19; i <= 120; i++){
+      numbers.push(i)
+      // console.log()
+    }
+    $('#randomNumber').text(Random);
+
+
+
+
+    $(".bin").on("click", function () {
+
+    });
