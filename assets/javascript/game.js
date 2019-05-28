7e29c761839641570f1c8ba183cbcad79376bcd9
Joanne Badua <joannebadua@Joannes-MacBook-Air.local>
2019-05-28 09:33:58 -0700

    set wins, losses, and binTotalPressed to zero, closed readyFunction tag

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,19 @@
+$(document).ready(function () {
+    var wins = 0;
+    var loss = 0;
+    var binTotal = 0;
 
-  $(document).ready(function () {
     // var firstBin = $("#1-paper");
     // var buttonValue =;
     // var userScore = "";
-    
+
     // Computer creates a random number (19-120) as a goal for the player to match 
     // with accumulated points
-    var Random = Math.floor(Math.random() * 120)+19;
+    var Random = Math.floor(Math.random() * 120) + 19;
     $('#randomNumber').text(Random);
     $("#binOne").on("click", function () {
-// need to add results when binOneTwoThreeFour clicked such as 
-// 1) adding Score inside and on the display.
-// 2) determining if this Score matches the Goal number
+        // need to add results when binOneTwoThreeFour clicked such as 
+        // 1) adding Score inside and on the display.
+        // 2) determining if this Score matches the Goal number
 
-    })
+    });
